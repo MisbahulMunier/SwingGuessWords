4e5479809735cf30370b17693db28369dfd2a48a
user <user@DESKTOP-492C8D4>
2017-10-02 16:21:19 +0700

    commit kedua

--- src/id/co/id/indocyber/guessgame/GuessingGame.java
+++ src/id/co/id/indocyber/guessgame/GuessingGame.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package id.co.id.indocyber.guessgame;
 
 import id.co.id.indocyber.guessgame.model.WordModel;
@@ -31,6 +27,7 @@ public class GuessingGame extends javax.swing.JFrame {
 //WordModel soal5=new WordModel("Gandaria City","Mall Anak Muda");
 //WordModel [] bankSoal={soal1,soal2,soal3,soal4,soal5};
     List<WordModel> bankSoal;
+    private String kata;
 
     /**
      * Creates new form GuessingGame
